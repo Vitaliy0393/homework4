dcedfe7f03c8e14e569643ee09eabb116b93e921
Vitaliy0393 <167925531+Vitaliy0393@users.noreply.github.com>
2024-05-05 23:48:35 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 'car', 'pensil')
+print(immutable_var)
+#immutable_var [0] = 'car' - кортеж не изменяемая . Можно только поменять список внутри кортежа
+#print(immutable_var)
+mutable_list = [1, 2, 'car', 'pensil']
+mutable_list [0] = 'car'
+print(mutable_list)
